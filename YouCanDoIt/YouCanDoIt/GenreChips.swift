f927054f93efad6ff170c9c68fb50007143dd06d
pig1606 <pig1606@knu.ac.kr>
2022-11-18 02:22:37 +0900

    [Design] Chip Color
    - When Chip is clicked, it changes its Color
    - But change together

--- YouCanDoIt/YouCanDoIt/GenreChips.swift
+++ YouCanDoIt/YouCanDoIt/GenreChips.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct GenreChips: View {
     
     @State var text = ""
+    @State var selected = false
+    @State var textColor: Color = Color("Red")
+    @State var capsuleColor: Color = Color("Blue")
     @State var chips: [[ChipData]] = [
         [ChipData(chipText: "All"), ChipData(chipText: "Sci-Fi"), ChipData(chipText: "Action"), ChipData(chipText: "Drama"), ChipData(chipText: "Thriller"), ChipData(chipText: "Music"), ChipData(chipText: "Adventure")]
     ]
@@ -17,21 +20,37 @@ struct GenreChips: View {
     var body: some View {
         
         ScrollView(.horizontal){
-            ForEach(chips.indices){index in
+            ForEach(chips.indices){ index in
                 HStack{
-                    ForEach(chips[index].indices){chipIndex in
+                    ForEach(chips[index].indices){ chipIndex in
                         Text(chips[index][chipIndex].chipText)
                             .padding(.horizontal, 8)
                             .padding(.vertical, 8)
-                            .foregroundColor(Color("Red"))
+                            .foregroundColor(textColor)
                             .fontWeight(.black)
                             .font(.system(size: 14))
-                            .background(Capsule().stroke(Color("Red"), lineWidth: 1))
+                            .background(Capsule()
+                                .strokeBorder(Color("Red"), lineWidth: 1)
+                                .background(Capsule().foregroundColor(capsuleColor)))
                             .lineLimit(1)
+                            .onTapGesture {
+                                self.selected.toggle()
+                                self.modifyColors()
+                            }
                     }
                     .padding(.vertical, 16)
                 }
             }
+        }
+    }
+    
+    func modifyColors() {
+        if selected {
+            textColor = .white
+            capsuleColor = Color("Red")
+        } else {
+            textColor = Color("Red")
+            capsuleColor = Color("Blue")
         }
     }
 }
